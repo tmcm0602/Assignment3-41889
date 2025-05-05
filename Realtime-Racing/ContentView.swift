214fa136a6c63cce8c4f220580d0ea87a2e09f04
tmcm0602 <126225860+tmcm0602@users.noreply.github.com>
2025-05-05 11:33:41 +1000

    Initial Commit

---
+++ Realtime-Racing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Realtime-Racing
+//
+//  Created by Admin on 5/5/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Realtime-Racing/Realtime_RacingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realtime_RacingApp.swift
+//  Realtime-Racing
+//
+//  Created by Admin on 5/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct Realtime_RacingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
